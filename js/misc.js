bb7aef945fd395630568befa1d3b53c48f8c45b0
Shaunak Ghosh <128846645+cloudmonster3@users.noreply.github.com>
2023-11-10 18:22:02 +0000

    Create a server function on express.js - misc.js
    add imports from node.js (express.js)

--- js/misc.js
+++ js/misc.js
@@ -1,3 +1,6 @@
+// IMPORTS
+import express from "express";
+
 // MISCELLANEOUS FUNCTIONS
 // print a debug log
 function print_debug(debug) {
@@ -7,4 +10,18 @@ function print_debug(debug) {
 		// In case of a error throw err. 
 		if (err) throw err; 
 	}) 
+}
+
+// create a static server with a name and port.
+function create_server(name, port, address) {
+	const app = express();
+	
+	app.get('/', (req, res) => {
+	  res.send('Welcome to the server!');
+	  res.send(name)
+	});
+	
+	app.listen(port, () => {
+	  console.log(`Server is running on port ${port} name ${name} address ${address}`);
+	});
 }
